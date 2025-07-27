a5e04a47e273d0d324206750f65211646ad162c5
AndrewTaker <n0thingg@yandex.ru>
2025-07-27 17:54:53 +0300

    remove manual auth calls

--- web/api_calls.go
+++ web/api_calls.go
@@ -141,50 +141,6 @@ type Resume struct {
 	IsScheduled  int
 }
 
-func HHGetToken(client *http.Client, code string) (*Token, error) {
-	req, err := http.NewRequest("POST", "https://api.hh.ru/token", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("HH-User-Agent", "n0thingg@yandex.ru update-cv")
-
-	q := req.URL.Query()
-	q.Add("client_id", clientID)
-	q.Add("client_secret", clientSecret)
-	q.Add("code", code)
-	q.Add("grant_type", "authorization_code")
-	q.Add("redirect_uri", redirectURL)
-	req.URL.RawQuery = q.Encode()
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		var hherror HHError
-		if err = json.NewDecoder(resp.Body).Decode(&hherror); err != nil {
-			return nil, fmt.Errorf("failed to decode nested token response: %w", err)
-		}
-		if hherror.Error == "invalid_grant" && hherror.ErrorDescription == "code has already been used" {
-			return nil, fmt.Errorf("invalud grant, returning")
-		}
-		bodyBytes, _ := io.ReadAll(resp.Body)
-		return nil, fmt.Errorf("bad status code getToken(): %d %s", resp.StatusCode, bodyBytes)
-	}
-
-	var token Token
-	if err := json.NewDecoder(resp.Body).Decode(&token); err != nil {
-		return nil, fmt.Errorf("failed to decode token response: %w", err)
-	}
-
-	return &token, nil
-
-}
-
 func HHGetUser(client *http.Client, t string) (*User, error) {
 	req, err := http.NewRequest("GET", "https://api.hh.ru/me", nil)
 	if err != nil {
@@ -245,28 +201,4 @@ func HHGetResumes(client *http.Client, t string) ([]Resume, error) {
 	}
 
 	return hhr.Items, nil
-}
-
-func HHInvalidateToken(client *http.Client, t string) error {
-	req, err := http.NewRequest("DELETE", "https://api.hh.ru/oauth/token", nil)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+t)
-	req.Header.Set("HH-User-Agent", "n0thingg@yandex.ru update-cv")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusNoContent {
-		bodyBytes, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("bad status code invalidateToken(): %d %s", resp.StatusCode, bodyBytes)
-	}
-
-	return nil
 }
