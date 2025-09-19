b648c6dc85107decf6d43a30218aec6c4918ad19
AndrewTaker <n0thingg@yandex.ru>
2025-09-19 21:51:16 +0300

    add token refresh dirty fix

--- scheduler/main.go
+++ scheduler/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"database/sql"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -15,26 +17,30 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+var err error
+
 func main() {
-	db, err := sql.Open("sqlite3", "./"+os.Getenv("DB_NAME"))
+	db, err = sql.Open("sqlite3", "./"+os.Getenv("DB_NAME"))
 	if err != nil {
 		log.Fatal("db err ", err)
 	}
 
 	query := `
-	select users.id, tokens.access_token, resumes.id, resumes.title
+	select users.id, tokens.access_token, tokens.refresh_token, resumes.id, resumes.title
 	from users
 	join tokens on users.id = tokens.user_id
 	join resumes on users.id = resumes.user_id;
 	`
 
 	type S struct {
-		UserID      string
-		AccessToken string
-		ResumeID    string
-		ResumeTitle string
-		Error       string
-		Timestamp   string
+		UserID       string
+		AccessToken  string
+		RefreshToken string
+		ResumeID     string
+		ResumeTitle  string
+		Error        string
+		Timestamp    string
 	}
 
 	rows, err := db.Query(query)
@@ -45,12 +51,16 @@ func main() {
 	var data []S
 	for rows.Next() {
 		var s S
-		var at string
-		if err := rows.Scan(&s.UserID, &at, &s.ResumeID, &s.ResumeTitle); err != nil {
+		var at, rt string
+		if err := rows.Scan(&s.UserID, &at, &rt, &s.ResumeID, &s.ResumeTitle); err != nil {
 			s.Error += err.Error()
 		}
 
 		s.AccessToken, err = Decrypt(at)
+		if err != nil {
+			s.Error += err.Error()
+		}
+		s.RefreshToken, err = Decrypt(at)
 		if err != nil {
 			s.Error += err.Error()
 		}
@@ -60,7 +70,7 @@ func main() {
 
 	client := &http.Client{Timeout: 15 * time.Second}
 	for _, u := range data {
-		timestamp, err := bump(client, u.AccessToken, u.ResumeID)
+		timestamp, err := bump(client, u.AccessToken, u.RefreshToken, u.ResumeID, u.UserID)
 		if err != nil {
 			u.Error = err.Error()
 		}
@@ -76,7 +86,7 @@ func main() {
 	}
 }
 
-func bump(client *http.Client, at, rid string) (string, error) {
+func bump(client *http.Client, at, rt, rid, uid string) (string, error) {
 	url := fmt.Sprintf("https://api.hh.ru/resumes/%s/publish", rid)
 	req, err := http.NewRequest(http.MethodPost, url, nil)
 	if err != nil {
@@ -98,10 +108,93 @@ func bump(client *http.Client, at, rid string) (string, error) {
 	}
 
 	if resp.StatusCode != http.StatusNoContent {
+		raw := make(map[string]any)
+		if err := json.Unmarshal(body, &raw); err != nil {
+			return "", err
+		}
+		if message, ok := raw["oauth_error"]; ok {
+			if message == "token-expired" {
+				accessToken, refreshToken, err := refreshToken(client, rt)
+				if err != nil {
+					return "", err
+				}
+				if err = updateToken(db, accessToken, refreshToken, uid); err != nil {
+					return "", err
+				}
+				if _, err := bump(client, accessToken, refreshToken, rid, uid); err != nil {
+					return "", err
+				}
+			}
+		}
 		return "", fmt.Errorf("non 204 returned" + string(body))
 	}
 
 	return time.Now().Format(time.RFC3339), nil
+}
+
+func updateToken(db *sql.DB, at, rt, uid string) error {
+	eat, err := Encrypt(at)
+	if err != nil {
+		return err
+	}
+
+	ert, err := Encrypt(rt)
+	if err != nil {
+		return err
+	}
+
+	query := `
+	update tokens 
+	set access_token = ?, refresh_token = ?
+	where user_id = ?
+	`
+
+	if _, err := db.Exec(query, eat, ert, uid); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func refreshToken(client *http.Client, rt string) (string, string, error) {
+	type Token struct {
+		AccessToken  string `json:"access_token"`
+		TokenType    string `json:"token_type"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresIn    uint   `json:"expires_in"`
+	}
+
+	req, err := http.NewRequest("POST", "https://api.hh.ru/token", nil)
+	if err != nil {
+		return "", "", err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("HH-User-Agent", "n0thingg@yandex.ru update-cv")
+
+	q := req.URL.Query()
+	q.Add("grant_type", "refresh_token")
+	q.Add("refresh_token", rt)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return "", "", fmt.Errorf("bad status code getToken(): %d %s", resp.StatusCode, bodyBytes)
+	}
+
+	var token Token
+	if err := json.NewDecoder(resp.Body).Decode(&token); err != nil {
+		return "", "", fmt.Errorf("failed to decode token response: %w", err)
+	}
+
+	return token.AccessToken, token.RefreshToken, nil
+
 }
 
 func Decrypt(encryptedString string) (string, error) {
@@ -133,4 +226,24 @@ func Decrypt(encryptedString string) (string, error) {
 	}
 
 	return string(plaintext), nil
+}
+
+func Encrypt(plaintext string) (string, error) {
+	block, err := aes.NewCipher([]byte(os.Getenv("ENCRYPTION_KEY")))
+	if err != nil {
+		return "", fmt.Errorf("could not create cipher block: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", fmt.Errorf("could not create GCM: %w", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", fmt.Errorf("could not generate nonce: %w", err)
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte(plaintext), nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
