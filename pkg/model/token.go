38d9e0c49e13db53491cad6b2423dbd9a8d06c75
AndrewTaker <n0thingg@yandex.ru>
2025-09-20 21:20:21 +0300

    pkg/model/ add encrypotion methods for token struct

--- pkg/model/token.go
+++ pkg/model/token.go
@@ -1,9 +1,39 @@
 package model
 
+import "pkg/utils"
+
 type Token struct {
 	AccessToken  string
 	RefreshToken string
 	ExpiresIn    uint
 	Code         string
 	TokenType    string
+}
+
+func (t *Token) Encrypt() error {
+	var err error
+
+	if t.AccessToken, err = utils.Encrypt(t.AccessToken); err != nil {
+		return err
+	}
+
+	if t.RefreshToken, err = utils.Encrypt(t.RefreshToken); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Token) Decrypt() error {
+	var err error
+
+	if t.AccessToken, err = utils.Decrypt(t.AccessToken); err != nil {
+		return err
+	}
+
+	if t.RefreshToken, err = utils.Decrypt(t.RefreshToken); err != nil {
+		return err
+	}
+
+	return nil
 }
