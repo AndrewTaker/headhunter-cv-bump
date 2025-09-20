b1f7e16372aca19361348047e1e6351ac35660d4
AndrewTaker <n0thingg@yandex.ru>
2025-09-20 21:00:01 +0300

    pkg/repository/ finish token repo

--- pkg/repository/token.go
+++ pkg/repository/token.go
@@ -1,8 +1,61 @@
 package repository
 
-import "pkg/model"
+import (
+	"database/sql"
+	"pkg/database"
+	"pkg/model"
+)
 
 type TokenRepository interface {
-	CreateOrUpdateToken(token model.Token, code, userID string) error
+	CreateOrUpdateToken(token *model.Token, code, userID string) error
 	GetTokenByUserID(userID string) (*model.Token, error)
+}
+
+type SqliteTokenRepository struct {
+	DB *database.DB
+}
+
+func NewSqliteTokenRepository(db *database.DB) TokenRepository {
+	return &SqliteTokenRepository{DB: db}
+}
+
+func (tr *SqliteTokenRepository) CreateOrUpdateToken(token *model.Token, code, userID string) error {
+	query := `
+	insert into tokens (access_token, refresh_token, expires_in, code, user_id) values (?, ?, ?, ?, ?)
+	on conflict(user_id) do update set
+	access_token = excluded.access_token,
+	refresh_token = excluded.refresh_token,
+	expires_in = excluded.expires_in,
+	code = excluded.code
+	`
+
+	if _, err := tr.DB.Exec(query,
+		token.AccessToken,
+		token.RefreshToken,
+		token.ExpiresIn,
+		code,
+		userID,
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (tr *SqliteTokenRepository) GetTokenByUserID(userID string) (*model.Token, error) {
+	query := `select access_token, refresh_token, expires_in from tokens where user_id = ?`
+
+	var t model.Token
+	if err := tr.DB.QueryRow(query, userID).Scan(
+		&t.AccessToken,
+		&t.RefreshToken,
+		&t.ExpiresIn,
+	); err != nil {
+		if err == sql.ErrNoRows {
+			return &model.Token{}, err
+		}
+		return nil, err
+	}
+
+	return &t, nil
 }
