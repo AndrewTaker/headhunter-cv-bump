c18a3857c4725df211fba24b75b3a04616f108d5
AndrewTaker <n0thingg@yandex.ru>
2025-09-20 20:20:10 +0300

    pkg/service/ adjust resume service

--- pkg/service/resume.go
+++ pkg/service/resume.go
@@ -1,10 +1,37 @@
 package service
 
-import "pkg/model"
+import (
+	"pkg/model"
+	"pkg/repository"
+)
 
 type ResumeService interface {
-	CreateOrUpdateResume(resumes []model.Resume, userID string) error
-	GetResumes(userID string) ([]model.Resume, error)
+	CreateOrUpdateResumes(resumes []model.Resume, userID string) error
+	GetUserResumes(userID string) ([]model.Resume, error)
 	GetResume(resumeID, userID string) (*model.Resume, error)
 	ToggleScheduling(resumeID, userID string, isScheduled bool) error
+}
+
+type ResumeServiceImpl struct {
+	resumeRepo repository.ResumeRepository
+}
+
+func NewResumeService(rr repository.ResumeRepository) ResumeService {
+	return &ResumeServiceImpl{resumeRepo: rr}
+}
+
+func (rs *ResumeServiceImpl) CreateOrUpdateResumes(resumes []model.Resume, userID string) error {
+	return rs.resumeRepo.CreateOrUpdateResumes(resumes, userID)
+}
+
+func (rs *ResumeServiceImpl) GetUserResumes(userID string) ([]model.Resume, error) {
+	return rs.resumeRepo.GetUserResumes(userID)
+}
+
+func (rs *ResumeServiceImpl) GetResume(resumeID, userID string) (*model.Resume, error) {
+	return rs.resumeRepo.GetResumeByID(resumeID, userID)
+}
+
+func (rs *ResumeServiceImpl) ToggleScheduling(resumeID, userID string, isScheduled bool) error {
+	return rs.resumeRepo.ToggleScheduling(resumeID, userID, isScheduled)
 }
