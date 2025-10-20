5b25f3605dc474bdf588d391bce6a6d39d0ed03b
AndrewTaker <n0thingg@yandex.ru>
2025-10-20 22:01:05 +0300

    add composables, components. add ui dir to air.toml

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useApi } from './composables/useApi.js'
+import  Resume  from './components/Resume.vue'
 
-const apiUrl = import.meta.env.VITE_API_URL
-const data = ref(null);
+const baseUrl = import.meta.env.VITE_API_URL
+const url = `${baseUrl}/resumes`
+const { data, error } = useApi(url)
 
 </script>
 
 <template>
-    {{ apiUrl }}
+    <Resume
+        v-for="resume in data"
+        :key="resume.ID"
+        :id="resume.ID"
+        :title="resume.Title"
+    />
 </template>
 
 <style scoped></style>
---
+++ ui/src/components/Resume.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { defineProps } from "vue"
+
+const props = defineProps(["id", "title"])
+</script>
+
+<template>
+    <h2>{{ props.title }}</h2>
+    <small>{{ props.id }}</small>
+</template>
+
+<style></style>
